31f303a4ce1086df3f33b391a565faaa3cbb9935
Eunice2000 <adediraneunice20@gmail.com>
2023-04-24 14:48:51 +0100

    server updated

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://yusufolatuji53:LEi65TxnW3yLTprz@memories.wtf14jp.mongodb.net/test'; 
+const CONNECTION_URL = 'mongodb+srv://Eunice_dev:Adebukola22@memories.y38ccmg.mongodb.net/test'; 
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
